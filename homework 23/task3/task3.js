6d33262e5b253a377ecc38411748b899c852d693
DESKTOP-4SO77QD\Анатолий <batstolya@gmail.com>
2021-04-08 15:20:03 +0300

    lesson 23 homework (task3) - done!

---
+++ homework 23/task3/task3.js
@@ -0,0 +1,9 @@
+let number = prompt ("Введите любое 3-х значное число!", 0)
+
+
+if (number[0] == number[1] || number[0] == number[2] ||  number[1] == number[2]  ) {
+    alert("true")
+} 
+else {
+    alert("false")
+}
